95e2002877b77e3e9d14da46aa164328cd211b05
Administrator <Administrator@USER-20170930NW>
2018-06-26 11:37:16 +0800

    课调模块功能 初始化提交

---
+++ src/main/java/com/briup/apps/poll/service/impl/SurveyServiceImpl.java
@@ -0,0 +1,63 @@
+package com.briup.apps.poll.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.briup.apps.poll.bean.Survey;
+import com.briup.apps.poll.bean.SurveyExample;
+import com.briup.apps.poll.dao.SurveyMapper;
+import com.briup.apps.poll.service.ISurveyService;
+
+@Service
+public class SurveyServiceImpl implements ISurveyService{
+	@Autowired
+    private SurveyMapper surveymapper; 
+	@Override
+	public List<Survey> findAllSurvey() throws Exception {
+		// TODO Auto-generated method stub
+		SurveyExample example=new SurveyExample();
+		return surveymapper.selectByExample(example);
+	}
+
+	@Override
+	public Survey findSurveyById(long id) throws Exception {
+		// TODO Auto-generated method stub
+		return surveymapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public List<Survey> querySurvey(String keywords) throws Exception {
+		// TODO Auto-generated method stub
+		SurveyExample example=new SurveyExample();
+		example.createCriteria().andCodeLike(keywords);
+		return surveymapper.selectByExample(example);
+	}
+
+	@Override
+	public void saveOrupdate(Survey survey) throws Exception {
+		// TODO Auto-generated method stub
+		if(survey.getId()!=null){
+			surveymapper.updateByPrimaryKeySelective(survey);
+		}else{
+			surveymapper.insert(survey);
+		}
+	}
+
+	@Override
+	public void deleteSurveyById(long id) throws Exception {
+		// TODO Auto-generated method stub
+		surveymapper.deleteByPrimaryKey(id);
+		
+	}
+
+	@Override
+	public void batchSurveyDelete(List<Long> ids) throws Exception {
+		// TODO Auto-generated method stub
+		for(long id : ids){
+			surveymapper.deleteByPrimaryKey(id);
+		}
+	}
+
+}

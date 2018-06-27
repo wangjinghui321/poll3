aad79ffefb2e31920de5d59ced4c391cb8008c88
阿辉 <@DESKTOP-36M6HJP>
2018-06-27 11:28:05 +0800

    questionVM

---
+++ src/main/java/com/briup/apps/poll/dao/extend/QuestionVMMapper.java
@@ -0,0 +1,14 @@
+package com.briup.apps.poll.dao.extend;
+
+import java.util.List;
+
+import com.briup.apps.poll.bean.extend.QuestionVM;
+
+public interface QuestionVMMapper {
+	/**
+	 * 查询所有
+	 * @return
+	 */
+	List<QuestionVM> selectAll();
+
+}
